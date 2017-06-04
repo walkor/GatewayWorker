55b734bb639538e74a45c92baa78d824ed224cdb
walkor <walkor@users.noreply.github.com>
2017-06-04 21:19:11 +0800

    print SQL when error occurs

--- src/Lib/DbConnection.php
+++ src/Lib/DbConnection.php
@@ -1761,7 +1761,10 @@ class DbConnection
                 }
             } else {
                 $this->rollBackTrans();
-                throw $e;
+                $msg = $e->getMessage();
+                $err_msg = "SQL:".$this->lastSQL()." ".$msg;
+                $exception = new \PDOException($err_msg, (int)$e->getCode());
+                throw $exception;
             }
         }
         $this->parameters = array();
